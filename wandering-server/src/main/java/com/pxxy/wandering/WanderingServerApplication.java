c07b90c3906c1c47cffafd54a9ea168104fee24f
ShenJinyong <438217638@qq.com>
2022-03-14 23:05:15 +0800

    添加后端服务，主体使用SpringBoot技术

---
+++ wandering-server/src/main/java/com/pxxy/wandering/WanderingServerApplication.java
@@ -0,0 +1,13 @@
+package com.pxxy.wandering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WanderingServerApplication.class, args);
+    }
+
+}
---
+++ wandering-server/src/test/java/com/pxxy/wandering/WanderingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pxxy.wandering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
